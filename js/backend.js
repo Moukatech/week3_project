9b240b9816f7379bbcf82053eeed4fceeb7c1524
Lewis Mocha <mocha@lewis.mocha>
2022-01-14 17:02:56 +0300

    add a function the js file and updated index file

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
-    $("img#img-des").click(function(){
-      $("p#wht-text0").toggle();
-      $("img#img-des").toggle();
+    $("img#img-design").click(function(){
+      $("p#design-dis").toggle();
+      $("img#img-design").toggle();
     });
+    $("img#img-dev").click(function(){
+      $("p#dev-dis").toggle();
+      $("img#img-dev").toggle();
+    });
+
+    $("img#img-product").click(function(){
+      $("p#product-dis").toggle();
+      $("img#img-product").toggle();
+    });
+
 });
